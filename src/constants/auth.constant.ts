4591c87f868eaca676d8015015f6edef2519f709
Jakub Ziemianski <47537468+Cubaa@users.noreply.github.com>
2022-12-09 02:24:42 +0100

    feat: [OS-6] Remove unused packages (#6)
    - remove redux-toolkit and redux packages, change cookie value

--- src/constants/auth.constant.ts
+++ src/constants/auth.constant.ts
@@ -2,4 +2,4 @@ export const TOKEN = "hgefugeuf763gyuwfdowjd8273ubnflmed";
 export const COOOKIE_NAME = "token";
 export const COOKIE_VALUE = `${encodeURIComponent(
   COOOKIE_NAME
-)}=${encodeURIComponent(TOKEN)}";expires=3600`;
+)}=${encodeURIComponent(TOKEN)}`;
